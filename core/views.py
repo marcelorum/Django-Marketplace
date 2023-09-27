631f007446369bacf543af2b54299993607c718f
Marcelo Nazar <marcelorum@gmail.com>
2023-09-27 19:00:51 -0300

    Login Django Set

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -12,3 +13,9 @@ def index(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def signup(request):
+    form = SignupForm()
+    return render(request, 'signup.html', {
+        'form': form
+    })
